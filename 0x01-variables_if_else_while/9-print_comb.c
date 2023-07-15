b3b70cf1245464e4f5a71ec35c409d8b8055eb73
Maureenkondile <tshimoloho66@gmail.com>
2023-07-15 11:12:24 -0700

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - start
+ *
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	int num;
+
+	for (i = 0; i <= 9; i++)
+	{
+		putchar((i % 10) + '0');
+		if (i == 9)
+			continue;
+		
+		putchar(',');
+		putchar(' ');
+	}
+
+	putchar('\n');
+
+	return (0);
+}
