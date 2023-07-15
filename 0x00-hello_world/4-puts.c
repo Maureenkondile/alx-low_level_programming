5923bdd18e0a2686af093bf7b3c23b2231f445e7
Maureenkondile <tshimoloho66@gmail.com>
2023-07-15 04:11:50 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#!/bin/bash
+#include <studio.h>
+/**
+* main - C programming
+*
+* Return: 0 (Success)
+*/
+int main(void)
+{
+	puts(Programming is like building a multilingual puzzle); return (0);
+}
