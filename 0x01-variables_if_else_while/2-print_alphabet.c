e03f029fe89aa2aabbb3f2fa907899b02ee5ae7f
Maureenkondile <tshimoloho66@gmail.com>
2023-07-15 10:34:03 -0700

    2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -8,8 +8,8 @@ int main(void)
 {
 	char ch = 'a';
 
-	printf ("Lowercase English Alphabets:\n");
-	while(ch <= 'z')
+	printf("Lowercase English Alphabets:\n");
+	while (ch <= 'z')
 	{
 		printf("%c ", ch);
 		ch++;
