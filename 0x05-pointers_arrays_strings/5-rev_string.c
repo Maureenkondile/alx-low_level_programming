b889610f2fe9410f07f324a0cefd778ff666e4ab
Maureenkondile <tshimoloho66@gmail.com>
2023-07-25 01:25:29 -0700

    5-rev_string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -4,16 +4,12 @@
  * rev_string - reverses a string
  * @str: string to be processed
  */
-void rev_string(char *str)
-{
-	int i, len;
-	char temp;
 
-	len = _strlen(str);
-	for (i = 0; i < len / 2; i++)
-	{
-		temp = str[i];
-		str[i] = str[len - 1 - i];
-		str[len - 1 - i] = temp;
-	}
+int _strlen(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+		;
+	return (i);
 }
