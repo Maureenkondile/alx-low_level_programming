9ca12c32c1055ba4aae797e7ec9600b2decf81ba
Maureenkondile <tshimoloho66@gmail.com>
2023-07-29 12:03:18 -0700

    100-rot13.c

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,40 @@
+#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * rot13 - Encodes a string using rot13.
+ * @str: The string to be encoded.
+ *
+ * Return: A pointer to the encoded string.
+*/
+
+char *rot13(const char *src)
+{
+    if(src == NULL){
+      return NULL;
+    }
+  
+    char* result = malloc(strlen(src));
+    
+    if(result != NULL){
+      strcpy(result, src);
+      char* current_char = result;
+      
+      while(*current_char != '\0'){
+        //Only increment alphabet characters
+        if((*current_char >= 97 && *current_char <= 122) || (*current_char >= 65 && *current_char <= 90)){
+          if(*current_char > 109 || (*current_char > 77 && *current_char < 91)){
+            //Characters that wrap around to the start of the alphabet
+            *current_char -= 13;
+          }else{
+            //Characters that can be safely incremented
+            *current_char += 13;
+          }
+        }
+        current_char++;
+      }
+    }
+    return result;
+}
